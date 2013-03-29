d71015d14cc1e02ffbe0e2d0651491e51b1da70b
Knut Zeissler <kzeissler@geomar.de>
2013-03-29 00:18:46 +0000

    Improved selfcheck script

--- selfcheck/runselfcheck.sh
+++ selfcheck/runselfcheck.sh
@@ -1,12 +1,42 @@
+#!/bin/bash
 
-cd selfcheck
-time .././model
-cd output
+# Move to project dir, e.g. /home/zzz/model
+# Only works from model or selfcheck directory
+if [ -L $0 ] ; then
+    ME=$(readlink $0)
+else
+    ME=$0
+fi
+DIR=$(dirname $ME)
+if [ "$DIR" = "." ]; then
+    cd .. 
+fi
+
+# Save paths
+modelDir="$PWD"
+selfcheckDir="$modelDir/selfcheck"
+buildDir="$modelDir/build"
+
+# List of output files to compare:
+testOutputPrefix="$selfcheckDir/output/new_000000000001_"
+referenceOutputPrefix="$selfcheckDir/output/reference_000000000001_"
+
+fileSuffixList="eta_out.nc psi_out.nc u_out.nc v_out.nc"
+
+# Delete previous test results
+for fileSuffix in ${fileSuffixList} 
+do
+rm -f $testOutputPrefix$fileSuffix
+done
+
+# Execute model binary from selfcheck dir
+cd $selfcheckDir
+time $buildDir/./model
+
+# Compare results
 set -x
-cdo diff reference_000000000001_eta_out.nc new_000000000001_eta_out.nc
-cdo diff reference_000000000001_psi_out.nc new_000000000001_psi_out.nc
-cdo diff reference_000000000001_u_out.nc new_000000000001_u_out.nc
-cdo diff reference_000000000001_v_out.nc new_000000000001_v_out.nc
-cdo diff reference_000000000001_u_mean_out.nc new_000000000001_u_mean_out.nc
-cdo diff reference_000000000001_v_mean_out.nc new_000000000001_v_mean_out.nc
+for fileSuffix in ${fileSuffixList} 
+do
+cdo diff $referenceOutputPrefix$fileSuffix $testOutputPrefix$fileSuffix
+done
 
