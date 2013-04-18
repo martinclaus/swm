5d8b13f85c7dd4d34613d4f3fb62c37c0eb91f65
Martin Claus <mclaus@geomar.de>
2013-04-18 12:37:01 +0000

    * Improved selfcheck routine. Every test has its own directory in selfcheck containing the namelist files and the header files you have adopted.

--- selfcheck/runselfcheck.sh
+++ selfcheck/runselfcheck.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# arguments
+# $1 test directory
+
 # Move to project dir, e.g. /home/zzz/model
 # Only works from model or selfcheck directory
 if [ -L $0 ] ; then
@@ -9,34 +12,33 @@ else
 fi
 DIR=$(dirname $ME)
 if [ "$DIR" = "." ]; then
-    cd .. 
+    cd ..
 fi
 
 # Save paths
 modelDir="$PWD"
 selfcheckDir="$modelDir/selfcheck"
+testDir="$selfcheckDir/$1"
 buildDir="$modelDir/build"
 
 # List of output files to compare:
-testOutputPrefix="$selfcheckDir/output/new_000000000001_"
-referenceOutputPrefix="$selfcheckDir/output/reference_000000000001_"
-
+testOutputPrefix="$testDir/output/new_000000000001_"
+referenceOutputPrefix="$testDir/output/reference_000000000001_"
 fileSuffixList="eta_out.nc psi_out.nc u_out.nc v_out.nc"
 
 # Delete previous test results
-for fileSuffix in ${fileSuffixList} 
+for fileSuffix in ${fileSuffixList}
 do
 rm -f $testOutputPrefix$fileSuffix
 done
 
-# Execute model binary from selfcheck dir
-cd $selfcheckDir
+# Execute model binary from test dir
+cd $testDir
 time $buildDir/./model
 
 # Compare results
 set -x
-for fileSuffix in ${fileSuffixList} 
+for fileSuffix in ${fileSuffixList}
 do
 cdo diff $referenceOutputPrefix$fileSuffix $testOutputPrefix$fileSuffix
 done
-
