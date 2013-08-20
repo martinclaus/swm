5963e8cbea967d4a97b920e65e48cb3772620ef3
Björn Peiler <bpeiler@geomar.de>
2013-08-20 08:53:01 +0000

    moved init, timestep and finish calls of model in own subroutines

--- src/model.f90
+++ src/model.f90
@@ -17,8 +17,6 @@
 !! @par Uses:
 !! vars_module, calc_lib, diag_module, domain_module,
 !! dynFromFile_module, swm_module, tracer_module
-!!
-!! @todo Put init, timestep and finish calls in a subroutine
 !------------------------------------------------------------------
 PROGRAM model
 #include "model.h"
@@ -39,131 +37,21 @@ PROGRAM model
 #endif
   IMPLICIT NONE
 
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! initialise io module (read output suffix and prefix from namelist)
-  !------------------------------------------------------------------
-  call initIO
-  print *, 'initIO done'
 
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! initialize the domain, indices, land masks
+  !! Call initialization routines
   !------------------------------------------------------------------
-  call initDomain
-  print *, 'initDomain done'
+  CALL initModel
 
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! initialize the variables (namelist input, allocation etc.)
+  !! Do the timestepping
   !------------------------------------------------------------------
-  call initVars
-  print *, 'initVars done'
+  CALL timestepModel
 
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! initialise Calc library
+  !! Call the finishing routines
   !------------------------------------------------------------------
-  call initCalcLib
-  print *, 'initCalcLib done'
-
-#ifdef DYNFROMFILE
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! initialise dynFromFile module
-  !------------------------------------------------------------------
-  call DFF_initDynFromFile
-  print *, 'DFF_initDynFromFile done'
-#endif
-
-#ifdef SWM
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! initializes the shallow water model
-  !------------------------------------------------------------------
-  call SWM_initSWM
-  print *, 'SWM_init done'
-#endif
-
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! Prepare output file (don't forget to close the files at the end of the programm)
-  !------------------------------------------------------------------
-  call initDiag
-  print *, 'initDiag done'
-
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! Initialise tracer field, timestepping coefficients and compute 2nd initial condition
-  !------------------------------------------------------------------
-#ifdef TRACER
-  call TRC_initTracer
-  print *, 'initTracer done'
-#endif
-
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! write initial fields to file
-  !------------------------------------------------------------------
-  call Diag
-  print *, 'first call of Diag done'
-
-  print *, 'starting integration'
-
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! Do the integration
-  !------------------------------------------------------------------
-  TIME: DO itt=1,Nt       ! loop over all time steps
-
-    !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    !! integrate modules to time itt*dt (stored as N0p1)
-    !------------------------------------------------------------------
-#ifdef DYNFROMFILE
-    call DFF_timestep
-#endif
-
-#ifdef SWM
-    call SWM_timestep
-#endif
-
-#ifdef TRACER
-    !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    !! time step tracer
-    !------------------------------------------------------------------
-    CALL TRC_tracerStep
-#endif
-
-    !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    !! shift model timestep
-    !------------------------------------------------------------------
-    CALL model_advance
-
-    !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    !! write fields to file and do diagnostics
-    !------------------------------------------------------------------
-    call Diag
-
-    !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    !! be verbose
-    !------------------------------------------------------------------
-    if (mod(REAL(itt), Nt / 100.) .LT. 1.) then
-      write (*, '(A,"itt = ",I10," (", F5.1,"% of ", I10,") done")', ADVANCE='NO') ACHAR(13),itt,100.0*itt/Nt,Nt
-    end if
-
-
-  END DO TIME
-
-#ifdef TRACER
-  call TRC_finishTracer
-#endif
-
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  !! Close opened files
-  !------------------------------------------------------------------
-  call finishDiag
-
-  call finishCalcLib
-
-#ifdef SWM
-  CALL SWM_finishSWM
-#endif
-
-#ifdef DYNFROMFILE
-  call DFF_finishDynFromFile
-#endif
-
-  call finishIO
+  CALL finishModel
 
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   ! Normal programm termination
@@ -171,6 +59,162 @@ PROGRAM model
   STOP 0
 
   CONTAINS
+
+
+
+    !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    !> @brief Calls initialization routines of the modules
+    !!
+    !! Calls the initialization routines of the used modules and prints
+    !! out information
+    !------------------------------------------------------------------
+    SUBROUTINE initModel
+      IMPLICIT NONE
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! initialise io module (read output suffix and prefix from namelist)
+        !------------------------------------------------------------------
+        call initIO
+        print *, 'initIO done'
+
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! initialize the domain, indices, land masks
+        !------------------------------------------------------------------
+        call initDomain
+        print *, 'initDomain done'
+
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! initialize the variables (namelist input, allocation etc.)
+        !------------------------------------------------------------------
+        call initVars
+        print *, 'initVars done'
+
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! initialise Calc library
+        !------------------------------------------------------------------
+        call initCalcLib
+        print *, 'initCalcLib done'
+
+#ifdef DYNFROMFILE
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! initialise dynFromFile module
+        !------------------------------------------------------------------
+        call DFF_initDynFromFile
+        print *, 'DFF_initDynFromFile done'
+#endif
+
+#ifdef SWM
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! initializes the shallow water model
+        !------------------------------------------------------------------
+        call SWM_initSWM
+        print *, 'SWM_init done'
+#endif
+
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! Prepare output file (don't forget to close the files at the end of the programm)
+        !------------------------------------------------------------------
+        call initDiag
+        print *, 'initDiag done'
+
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! Initialise tracer field, timestepping coefficients and compute 2nd initial condition
+        !------------------------------------------------------------------
+#ifdef TRACER
+        call TRC_initTracer
+        print *, 'initTracer done'
+#endif
+
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! write initial fields to file
+        !------------------------------------------------------------------
+        call Diag
+        print *, 'first call of Diag done'
+
+    END SUBROUTINE initModel
+
+
+    !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    !> @brief Calls the timestepping routines of the modules
+    !!
+    !! Loops over all time steps, calls the timestepping routines of the used
+    !! modules and calls diagnostic routines
+    !------------------------------------------------------------------------
+    SUBROUTINE timestepModel
+      IMPLICIT NONE
+        print *, 'starting integration'
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! Do the integration
+        !------------------------------------------------------------------
+        TIME: DO itt=1,Nt       ! loop over all time steps
+
+          !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+          !! integrate modules to time itt*dt (stored as N0p1)
+          !------------------------------------------------------------------
+#ifdef DYNFROMFILE
+          call DFF_timestep
+#endif
+
+#ifdef SWM
+          call SWM_timestep
+#endif
+
+#ifdef TRACER
+          !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+          !! time step tracer
+          !------------------------------------------------------------------
+          call TRC_tracerStep
+#endif
+
+          !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+          !! shift model timestep
+          !------------------------------------------------------------------
+          call model_advance
+
+          !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+          !! write fields to file and do diagnostics
+          !------------------------------------------------------------------
+          call Diag
+
+          !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+          !! be verbose
+          !------------------------------------------------------------------
+          if (mod(REAL(itt), Nt / 100.) .LT. 1.) then
+            write (*, '(A,"itt = ",I10," (", F5.1,"% of ", I10,") done")', ADVANCE='NO') ACHAR(13),itt,100.0*itt/Nt,Nt
+          end if
+
+        END DO TIME
+    END SUBROUTINE timestepModel
+
+
+    !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    !> @brief Calls the finishing routines of the modules
+    !!
+    !! Calls the finishing routines of the used modules
+    !--------------------------------------------------------------------------
+    SUBROUTINE finishModel
+      IMPLICIT NONE
+
+#ifdef TRACER
+        call TRC_finishTracer
+#endif
+
+        !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        !! Close opened files
+        !------------------------------------------------------------------
+        call finishDiag
+      
+        call finishCalcLib
+
+#ifdef SWM
+        call SWM_finishSWM
+#endif
+
+#ifdef DYNFROMFILE
+        call DFF_finishDynFromFile
+#endif
+
+        call finishIO
+    END SUBROUTINE finishModel
 
     !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     !> @brief Calls advance routines of the modules
