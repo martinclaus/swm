c7785298230d35fc83b37659aec6acbf21b0b8aa
Martin Claus <mclaus@geomar.de>
2017-02-16 14:11:40 +0100

    vars_module is now using types module

--- src/types.f90
+++ src/types.f90
@@ -6,5 +6,6 @@ module types
     INTEGER, public, parameter  :: KINT = INT_KIND
     INTEGER, public, parameter  :: KSHORT = SHORT_KIND
     INTEGER, public, parameter  :: KDOUBLE = DOUBLE_KIND
+    INTEGER, public, parameter  :: KINT_ITT = INT_ITT_KIND
 
 end module types
