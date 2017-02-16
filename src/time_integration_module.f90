01f3cab03ea690b04781cf261daacec19c5982c2
Martin Claus <mclaus@geomar.de>
2017-02-16 15:00:47 +0100

    swm modules now use types module

--- src/time_integration_module.f90
+++ src/time_integration_module.f90
@@ -9,13 +9,14 @@
 !------------------------------------------------------------------
 MODULE time_integration_module
 #include "io.h"
+  use types
   implicit none
   private
 
   public :: time_integration_init, integrate_AB
 
-  real(8), dimension(5, 5) :: ab_coeff     !< Coefficient matrix for AB schemes up to 5th level
-  real(8)                  :: ab_chi=.1_8  !< Displacement coefficient for 2nd-level AB schemes
+  real(KDOUBLE), dimension(5, 5) :: ab_coeff     !< Coefficient matrix for AB schemes up to 5th level
+  real(KDOUBLE)                  :: ab_chi=.1_KDOUBLE  !< Displacement coefficient for 2nd-level AB schemes
 
 contains
 
@@ -27,19 +28,20 @@ contains
   !------------------------------------------------------------------
   subroutine time_integration_init()
     namelist / tint_nl / ab_chi
-    integer :: stat
+    integer(KINT) :: stat
 
     ! read namelist
     open(UNIT_MODEL_NL, file = MODEL_NL)
     read(UNIT_MODEL_NL, nml = tint_nl, iostat=stat)
     close(UNIT_MODEL_NL)
     
-    ab_coeff = 0._8
-    ab_coeff(1, 1) = 1._8
-    ab_coeff(1:2, 2) = (/-0.5_8 - ab_chi, 1.5_8 + ab_chi /)
-    ab_coeff(1:3, 3) = (/ 5._8 / 12._8, -16._8 / 12._8, 23._8 / 12._8 /)
-    ab_coeff(1:4, 4) = (/-3._8/8._8, 37._8/24._8, -59._8/24._8, 55._8/24._8 /)
-    ab_coeff(1:5, 5) = (/ 251._8/720._8, -637._8/360._8, 109._8/30._8, -1387._8/360._8, 1901._8/720._8 /)
+    ab_coeff = 0._KDOUBLE
+    ab_coeff(1, 1) = 1._KDOUBLE
+    ab_coeff(1:2, 2) = (/-0.5_KDOUBLE - ab_chi, 1.5_KDOUBLE + ab_chi /)
+    ab_coeff(1:3, 3) = (/ 5._KDOUBLE / 12._KDOUBLE, -16._KDOUBLE / 12._KDOUBLE, 23._KDOUBLE / 12._KDOUBLE /)
+    ab_coeff(1:4, 4) = (/-3._KDOUBLE/8._KDOUBLE, 37._KDOUBLE/24._KDOUBLE, -59._KDOUBLE/24._KDOUBLE, 55._KDOUBLE/24._KDOUBLE /)
+    ab_coeff(1:5, 5) = (/ 251._KDOUBLE/720._KDOUBLE, -637._KDOUBLE/360._KDOUBLE, 109._KDOUBLE/30._KDOUBLE, &
+                         -1387._KDOUBLE/360._KDOUBLE, 1901._KDOUBLE/720._KDOUBLE /)
 
   end subroutine time_integration_init
 
@@ -53,12 +55,12 @@ contains
   !! @par Uses:
   !! vars_module, ONLY : dt,itt
   !------------------------------------------------------------------
-  real(8) function integrate_AB(A_N, G, impl, order) result(A_NP1)
+  real(KDOUBLE) function integrate_AB(A_N, G, impl, order) result(A_NP1)
     USE vars_module, ONLY : dt, itt
     IMPLICIT NONE
-    REAL(8), intent(in) :: G(:), A_N, impl
-    INTEGER             :: tstep
-    integer             :: order                 !< order of the integration scheme
+    real(KDOUBLE), intent(in) :: G(:), A_N, impl
+    integer(KINT)             :: tstep
+    integer(KINT)             :: order                 !< order of the integration scheme
     
     tstep = min(order, itt)
     A_NP1 = (A_N + dt * dot_product(ab_coeff(1:tstep, tstep), G(1:tstep))) / impl
