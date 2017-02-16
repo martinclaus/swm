f939da1f22ccb4afd4146b6020b37b7ecded57f1
Martin Claus <mclaus@geomar.de>
2017-02-16 15:23:44 +0100

    updated types.h

--- include/types.h
+++ include/types.h
@@ -5,14 +5,22 @@
 #ifndef FILE_TYPE_SEEN
 #define FILE_TYPE_SEEN
 
+/* default integer kind */
 #if ! defined(INT_KIND)
     #define INT_KIND 4
 #endif
 
+/* kind for mask arrays */
 #if ! defined(SHORT_KIND)
     #define SHORT_KIND 1
 #endif
 
+/* Kind of time index */
+#if ! defined(INT_ITT_KIND)
+    #define INT_ITT_KIND 8
+#endif
+
+/* working precision of real variables */
 #if ! defined(DOUBEL_KIND)
     #define DOUBLE_KIND 8
 #endif
