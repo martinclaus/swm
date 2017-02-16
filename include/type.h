410f11092e2bf9d959c4f9bea5b1b51ba63067d3
Martin Claus <mclaus@geomar.de>
2017-02-16 10:27:53 +0100

    created header file defining default kind values

---
+++ include/type.h
@@ -0,0 +1,20 @@
+/*
+ * Define default data types
+*/
+
+#ifndef FILE_TYPE_SEEN
+#define FILE_TYPE_SEEN
+
+#if ! defined(INT_KIND)
+    #define INT_KIND 4
+#endif
+
+#if ! defined(SHORT_KIND)
+    #define SHORT_KIND 1
+#endif
+
+#if ! defined(DOUBEL_KIND)
+    #define DOUBLE_KIND 8
+#endif
+
+#endif
