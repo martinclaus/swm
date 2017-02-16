710d0b4b00f5fff03bf6aeaae26cc2500aba513f
Martin Claus <mclaus@geomar.de>
2017-02-16 11:23:25 +0100

    added types module defining kind values fir basic data types

---
+++ src/types.f90
@@ -0,0 +1,10 @@
+module types
+    implicit none
+    private
+    #include "types.h"
+
+    INTEGER, public, parameter  :: KINT = INT_KIND
+    INTEGER, public, parameter  :: KSHORT = SHORT_KIND
+    INTEGER, public, parameter  :: KDOUBLE = DOUBLE_KIND
+
+end module types
