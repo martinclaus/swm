3969f896ebc78b86088bef35df51dbb8b102bfa8
Martin Claus <mclaus@geomar.de>
2018-11-02 10:47:31 +0100

    Added test case to meassure IO independent performance

---
+++ selfcheck/test_performance/model.h
@@ -0,0 +1,42 @@
+/*
+Header file for shallow water model. Meant to set different physics.
+ */
+#ifndef FILE_MODEL_SEEN
+#define FILE_MODEL_SEEN
+
+/* OpenMP */
+#define OMP_COLLAPSE(N) collapse(max(1, N-1))
+#define OMPCHUNK Nx
+/*#define OMPSCHEDULE GUIDED*/
+#define OMPSCHEDULE STATIC
+
+/* Switch for Shallow Water Model */
+#define SWM
+#define BAROTROPIC
+#define FULLY_NONLINEAR
+/* Switches for timestepping (only use one at a time) */
+#define SWM_TSTEP_ADAMSBASHFORTH
+/* #define SWM_TSTEP_HEAPS */
+/* Switches for forcing */
+/* #define TAU_SCALE 1e-2 */
+/* Switches for damping */
+#define QUADRATIC_BOTTOM_FRICTION
+#define LATERAL_MIXING
+
+/* Sponge layers */
+#define NEWTONIAN_SPONGE "NSEW"
+#define VELOCITY_SPONGE "NSEW"
+
+
+/* Switch to load dynamical variables from file (mean flow) */
+/* #define DYNFROMFILE */
+
+#define H_OVERWRITE_DEF 0.
+
+/* Possible calculation of Coriolis-Parameter */
+#define CORIOLIS_NOF 0
+#define CORIOLIS_FPLANE 1
+#define CORIOLIS_BETAPLANE 2
+#define CORIOLIS_SPHERICALGEOMETRY 3
+
+#endif
