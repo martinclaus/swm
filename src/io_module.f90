3e6cf7d3b61ab98126433d1a2056b470c4db694b
Martin Claus <mclaus@geomar.de>
2023-01-30 12:39:22 +0100

    Create abstract reader/writer API

--- src/io_module.f90
+++ src/io_module.f90
@@ -21,7 +21,7 @@ MODULE io_module
   use list_mod, only: List, ListIterator
   IMPLICIT NONE
   private
-  public :: Io, IoHandle, HandleArgs
+  public :: Io, Reader, Writer, HandleArgs
   
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   !> @brief  Abstract base type for all IO related types
@@ -39,31 +39,48 @@ MODULE io_module
     procedure :: finalize => finishIO
     procedure :: step => do_nothing_io
     procedure :: advance => do_nothing_io
-    procedure(iGetHandle), deferred :: get_handle
+    procedure(iGetReader), deferred :: get_reader
+    procedure(iGetWriter), deferred :: get_writer
     procedure(iNoArg), deferred :: init, finish
-    procedure(iCreateDSEuler), deferred, private :: createDSEuler
-    procedure(iCreateDSLagrange), deferred, private :: createDSLagrange
-    procedure(iGetVar3Dhandle), deferred, private :: getVar3Dhandle
-    procedure(iGetVar2Dhandle), deferred, private :: getVar2Dhandle
-    procedure(iGetVar1Dhandle), deferred, private :: getVar1Dhandle
-    procedure(iPutVarEuler), deferred, private :: putVarEuler
-    procedure(iPutVarLagrange), deferred, private :: putVarLagrange
-    generic :: createDS => createDSEuler, createDSLagrange  !< creates a dataset with a given grid
-    generic :: getVar => getVar3Dhandle, getVar2Dhandle, getVar1Dhandle  !< Read time slice of a variable from a dataset
-    generic :: putVar => putVarEuler, putVarLagrange !< Write time slice of a variable to a dataset
   end type Io
   
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   !> @brief  Abstract base type for all handles related to IO
   !!
-  !! A IOHandle may be a file, a variable inside a file or a
-  !! network socket to send data to.
+  !! A IOHandle may be a reading or writing handle to a file, a variable
+  !! inside a file or a network socket to send data to.
   !------------------------------------------------------------------
   type, abstract :: IoHandle
+    class(Io), pointer      :: io_comp => null()
     type(calendar)          :: calendar              !< Calendar of time data received from or send to the handle.
     real(KDOUBLE)           :: missval=MISS_VAL_DEF  !< Value to flag missing data
     LOGICAL                 :: isOpen = .FALSE.      !< Flag, if handle is ready to read or write data
   end type IoHandle
+
+  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  !> @brief  Abstract base type for all reading IO handles
+  !------------------------------------------------------------------
+  type, abstract, extends(IoHandle) :: Reader
+  contains
+    procedure(iReadInitialCondition), deferred :: read_initial_conditions
+    procedure(iGetVar3Dhandle), deferred, private :: getVar3Dhandle
+    procedure(iGetVar2Dhandle), deferred, private :: getVar2Dhandle
+    procedure(iGetVar1Dhandle), deferred, private :: getVar1Dhandle
+    generic :: getVar => getVar3Dhandle, getVar2Dhandle, getVar1Dhandle  !< Read time slice of a variable from a dataset
+  end type Reader
+
+  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+  !> @brief  Abstract base type for all writing IO handles
+  !------------------------------------------------------------------
+  type, abstract, extends(IoHandle) :: Writer
+  contains
+    procedure(iCreateDSEuler), deferred, private :: createDSEuler
+    procedure(iCreateDSLagrange), deferred, private :: createDSLagrange
+    procedure(iPutVarEuler), deferred, private :: putVarEuler
+    procedure(iPutVarLagrange), deferred, private :: putVarLagrange
+    generic :: createDS => createDSEuler, createDSLagrange  !< creates a dataset with a given grid
+    generic :: putVar => putVarEuler, putVarLagrange !< Write time slice of a variable to a dataset
+  end type Writer
 
   !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   !> @brief  Type to collect arguments for creating a handle
@@ -92,63 +109,69 @@ MODULE io_module
   end interface KeyValue
 
   abstract interface
-    function iGetHandle(self, args) result(handle)
-      import Io, HandleArgs, IoHandle
+    function iGetReader(self, args) result(handle)
+      import Io, HandleArgs, Reader
       class(Io), intent(in)            :: self
       class(HandleArgs), intent(inout) :: args
-      class(IoHandle), allocatable     :: handle
+      class(Reader), allocatable       :: handle
+    end function
+    function iGetWriter(self, args) result(handle)
+      import Io, HandleArgs, Writer
+      class(Io), intent(in)            :: self
+      class(HandleArgs), intent(inout) :: args
+      class(Writer), allocatable       :: handle
     end function
     subroutine iNoArg(self)
       import Io
       class(Io), intent(inout) :: self
     end subroutine iNoArg
-    subroutine iCreateDSEuler(self, handle, grid)
-      import Io, IoHandle, grid_t
-      class(Io)                         :: self
-      class(IoHandle), INTENT(inout)    :: handle   !< Initialised IoHandle
-      TYPE(grid_t), POINTER, INTENT(in) :: grid     !< Spatial grid used to create dataset
-    end subroutine iCreateDSEuler
-    subroutine iCreateDSLagrange(self, handle, grid)
-      import Io, IoHandle, t_grid_lagrange
-      class(Io)                         :: self
-      class(IoHandle), INTENT(inout)    :: handle   !< Initialised IoHandle
-      TYPE(t_grid_lagrange), POINTER, INTENT(in) :: grid     !< Spatial grid used to create dataset
-    end subroutine iCreateDSLagrange
-    SUBROUTINE iGetVar3Dhandle(self, handle, var, tstart, missmask)
-      import Io, IoHandle, KINT, KDOUBLE, KSHORT
-      class(Io), intent(in)                                    :: self
-      class(IoHandle), INTENT(inout)                           :: handle    !< File handle pointing to the variable to read from
+
+    subroutine iReadInitialCondition(self, var, missmask)
+      import Reader, KDOUBLE, KSHORT
+      class(Reader), intent(in)             :: self
+      real(KDOUBLE), DIMENSION(:,:), INTENT(out)             :: var      !< Data to return
+      integer(KSHORT), DIMENSION(:,:), OPTIONAL, INTENT(out) :: missmask !< missing value mask
+    end subroutine iReadInitialCondition
+    SUBROUTINE iGetVar3Dhandle(self, var, tstart, missmask)
+      import Reader, KINT, KDOUBLE, KSHORT
+      class(Reader), INTENT(inout)                             :: self    !< File handle pointing to the variable to read from
       integer(KINT), INTENT(in)                                :: tstart    !< Time index to start reading
       real(KDOUBLE), DIMENSION(:,:,:), INTENT(out)             :: var       !< Data read from disk
       integer(KSHORT), DIMENSION(:,:,:), OPTIONAL, INTENT(out) :: missmask  !< Missing value mask
     end subroutine
-    SUBROUTINE iGetVar2Dhandle(self, handle, var, tstart, missmask)
-      import Io, IoHandle, KINT, KDOUBLE, KSHORT
-      class(Io), intent(in)                                    :: self
-      class(IoHandle), INTENT(inout)                           :: handle    !< File handle pointing to the variable to read from
+    SUBROUTINE iGetVar2Dhandle(self, var, tstart, missmask)
+      import Reader, KINT, KDOUBLE, KSHORT
+      class(Reader), intent(in)                                :: self
       integer(KINT), INTENT(in)                                :: tstart    !< Time index to start reading
       real(KDOUBLE), DIMENSION(:,:), INTENT(out)               :: var       !< Data read from disk
       integer(KSHORT), DIMENSION(:,:), OPTIONAL, INTENT(out)   :: missmask  !< Missing value mask
     end subroutine
-    SUBROUTINE iGetVar1Dhandle(self, handle, var, tstart)
-      import Io, IoHandle, KINT, KDOUBLE, KSHORT
-      class(Io), intent(in)                      :: self
-      class(IoHandle), INTENT(inout)             :: handle  !< File handle locating the variable to read
-      real(KDOUBLE), DIMENSION(:), INTENT(out)   :: var     !< Data read from disk
-      integer(KINT), INTENT(in), OPTIONAL        :: tstart  !< Index to start at
+    SUBROUTINE iGetVar1Dhandle(self, var, tstart)
+      import Reader, KINT, KDOUBLE, KSHORT
+      class(Reader), intent(in)                :: self
+      real(KDOUBLE), DIMENSION(:), INTENT(out) :: var     !< Data read from disk
+      integer(KINT), INTENT(in), OPTIONAL      :: tstart  !< Index to start at
     end subroutine
-    subroutine iPutVarEuler(self, handle, varData, time, grid)
-      import Io, IoHandle, grid_t, KDOUBLE
-      class(Io), intent(in)                     :: self
-      class(IoHandle), INTENT(inout)            :: handle        !< Initialised file handle pointing to the variable to write data to
+    subroutine iCreateDSEuler(self, grid)
+      import Writer, grid_t
+      class(Writer)                     :: self
+      TYPE(grid_t), POINTER, INTENT(in) :: grid     !< Spatial grid used to create dataset
+    end subroutine iCreateDSEuler
+    subroutine iCreateDSLagrange(self, grid)
+      import Writer, t_grid_lagrange
+      class(Writer)                     :: self
+      TYPE(t_grid_lagrange), POINTER, INTENT(in) :: grid     !< Spatial grid used to create dataset
+    end subroutine iCreateDSLagrange
+    subroutine iPutVarEuler(self, varData, time, grid)
+      import Writer, grid_t, KDOUBLE
+      class(Writer), intent(in)                 :: self
       real(KDOUBLE), DIMENSION(:,:), INTENT(in) :: varData   !< Data to write
       type(grid_t), intent(in), optional        :: grid      !< Grid information
       real(KDOUBLE), INTENT(in), OPTIONAL       :: time      !< Time coordinate of time slice
     end subroutine
-    subroutine iPutVarLagrange(self, handle, varData, time, grid)
-      import Io, IoHandle, t_grid_lagrange, KDOUBLE
-      class(Io), intent(in)                       :: self
-      class(IoHandle), INTENT(inout)              :: handle        !< Initialised file handle pointing to the variable to write data to
+    subroutine iPutVarLagrange(self, varData, time, grid)
+      import Writer, t_grid_lagrange, KDOUBLE
+      class(Writer), intent(in)                   :: self
       real(KDOUBLE), DIMENSION(:), INTENT(in)     :: varData   !< Data to write
       type(t_grid_lagrange), intent(in), optional :: grid      !< Grid information
       real(KDOUBLE), INTENT(in), OPTIONAL         :: time      !< Time coordinate of time slice
